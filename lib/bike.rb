09226f5075fe7c2f10e2a3c577a8a19acf508ac8
carlostrapet <trapet_llamas@hotmail.com>
2018-01-26 15:42:09 +0000

    refactored working? method

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,13 +1,31 @@
 class Bike
 
-  attr_reader :state
+  # attr_reader :state
 
-  def initialize(state = "not_broken")
-    @state = state
+  # def initialize(state = "not_broken")
+  #   @state = state
+  # end
+
+  # def working?
+  #   if @state == "broken"
+  #     false
+  #   else
+  #     true
+  #   end
+  # end
+
+  # Walkthrough has a different approach; creating a method
+  # to report the bike as broken (eg. bike.report_broken)
+def report_broken
+  @broken = true
+end
+
+  def broken?
+    @broken
   end
 
   def working?
-    if @state == "broken"
+    if self.broken?
       false
     else
       true
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,9 +2,16 @@ require 'bike'
 
 describe Bike do
   it {is_expected.to respond_to :working?}
-  it 'responds to working? correctly' do 
-    bike = Bike.new("broken")
-    expect(bike.working?).to eq false
-  end
+  # it 'responds to working? correctly' do 
+  #   bike = Bike.new("broken")
+  #   expect(bike.working?).to eq false
+  # end
 
+  # Walkthrough has a different approach; creating a method
+  # to report the bike as broken (eg. bike.report_broken)
+
+  it 'can be reported broken' do
+    subject.report_broken
+    expect(subject).to be_broken
+  end
 end
